7c698dc275e53bb19cd8adb82428954e2f7b3b9b
Wolfgang <wolfgang@walznet.de>
2018-12-01 11:48:57 +0100

    initial commit

---
+++ app/auth/forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+
+
+class RegistrationForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired(), Length(3,15, message='between 3 to 15 characters')])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(5), EqualTo('confirm', message='password required, minLen=5')])
+    confirm = PasswordField('Confirm', validators=[DataRequired()])
+    submit = SubmitField('Register')
---
+++ app/auth/routes.py
@@ -0,0 +1,22 @@
+from flask import render_template, request, flash, redirect, url_for
+from app.auth.forms import RegistrationForm
+from app.auth import authentication as at
+from app.auth.models import User
+
+
+@at.route('/register', methods=['GET', 'POST'])
+def register_user():
+
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        User.create_user(
+            user=form.name.data,
+            email=form.emal.data,
+            password=form.password.data
+        )
+        flash('Registration Successful')
+        return redirect(url_for('at.login_user'))
+
+
+    return render_template('registration.html', form=form)
+
