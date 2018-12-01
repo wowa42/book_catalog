4d4b0146a1837170d94ff9c749edded3dfba6339
Wolfgang <wolfgang@walznet.de>
2018-12-01 12:45:13 +0100

    registration

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -11,12 +11,13 @@ def register_user():
     if form.validate_on_submit():
         User.create_user(
             user=form.name.data,
-            email=form.emal.data,
-            password=form.password.data
-        )
+            email=form.email.data,
+            password=form.password.data)
         flash('Registration Successful')
-        return redirect(url_for('at.login_user'))
-
-
+        return redirect(url_for('authentication.login_user'))
     return render_template('registration.html', form=form)
 
+
+@at.route('/login', methods=['GET', 'POST'])
+def login_user():
+    return render_template('login.html')
